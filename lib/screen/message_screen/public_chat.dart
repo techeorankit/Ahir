b9482cfe026bccf56f5a90fc5c084e5ab81ba0b1
HP <techeor.ankit@gmail.com>
2025-12-17 18:17:03 +0530

    video image upload in group

--- lib/screen/message_screen/public_chat.dart
+++ lib/screen/message_screen/public_chat.dart
@@ -1,9 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
-
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart' as html_core;  // Alias the flutter_widget_from_html_core
+import 'package:image_picker/image_picker.dart' as image_picker_lib;  // Alias the image_picker
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:shortzz/common/functions/media_picker_helper.dart';
+import 'package:video_player/video_player.dart';
+import '../../common/manager/logger.dart';
 import '../../common/manager/session_manager.dart';
+import '../../common/service/api/common_service.dart';
+import '../../utilities/const_res.dart';
+
 
 class PublicChatScreen extends StatefulWidget {
   final String groupId; // use camelCase for variables
@@ -51,16 +59,102 @@ class _ChatScreenState extends State<PublicChatScreen> {
     }
 
   }
+  void _sendImage(String image) async {
+
+    if (image.isNotEmpty) {
+      await _firestore.collection('groups').doc(widget.groupId).collection('messages').add({
+        'text': '',
+        'imageVideo': image,
+        'sender': userId ?? 'Anonymous',
+        'name': name ?? 'Anonymous',
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+      _controller.clear();
+      _scrollToBottom();
+    }
+
+  }
 
   void _scrollToBottom() {
-    Future.delayed(const Duration(milliseconds: 100), () {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
+    Future.delayed(const Duration(milliseconds: 300), () {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
     });
   }
+
+
+// Assuming your existing CommonService and Loggers are set up correctly
+  Future<void> imageUpload() async {
+    // Pick image or video
+    final image_picker_lib.ImagePicker _picker = image_picker_lib.ImagePicker();
+    XFile? pickedFile;
+
+    // Show options to pick either an image or a video
+    final pickedMedia = await showDialog<MediaType>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Pick Media", style: TextStyle(color: Colors.black)),
+            actions: <Widget>[
+            TextButton(
+              child: Text("Pick Image"),
+              onPressed: () => Navigator.of(context).pop(MediaType.image),
+            ),
+            TextButton(
+              child: Text("Pick Video"),
+              onPressed: () => Navigator.of(context).pop(MediaType.video),
+            ),
+          ],
+        );
+      },
+    );
+
+    // If user cancels the dialog
+    if (pickedMedia == null) {
+      return;
+    }
+
+    try {
+      if (pickedMedia == MediaType.image) {
+        pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+      } else if (pickedMedia == MediaType.video) {
+        pickedFile = await _picker.pickVideo(source: ImageSource.gallery);
+      }
+
+      if (pickedFile != null) {
+        // Proceed to upload the selected media
+        await CommonService.instance
+            .uploadFileGivePath(XFile(pickedFile.path))
+            .then((result) {
+          if (result.status == true && result.data != null) {
+            // uploadedImagePaths.add(result.data!);
+            Loggers.success('Media uploaded: ${result.data}');
+
+            _sendImage(result.data.toString());
+          } else {
+            Loggers.error('Media upload failed: ${result.message}');
+            // deleteFiles([pickedFile.path]);
+            return Loggers.error('Media upload failed: ${result.message}');
+          }
+        });
+      } else {
+        Loggers.error('No media selected');
+      }
+    } catch (e) {
+      Loggers.error('Error uploading media: $e');
+      return Loggers.error('Error uploading media: $e');
+    }
+  }
+
+// Enum for media type
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -95,18 +189,22 @@ class _ChatScreenState extends State<PublicChatScreen> {
                 if (!snapshot.hasData) {
                   return const Center(child: CircularProgressIndicator());
                 }
-
+                _scrollToBottom();
                 final messages = snapshot.data!.docs;
                 return ListView.builder(
                   controller: _scrollController,
                   itemCount: messages.length,
                   itemBuilder: (context, index) {
                     final message = messages[index];
-                    final text = message['text'];
-                    final sender = message['sender'];
-                    final name = message['name'];
+                    final data = message.data() as Map<String, dynamic>;
+
+                    final String text = data['text'] ?? '';
+                    final int sender = data['sender'] ?? '';
+                    final String name = data['name'] ?? '';
+                    final String imageVideo = data['imageVideo'] ?? '';
+
                     final isMe = sender == userId;
-                    _scrollToBottom();
+
                     return Container(
                       margin: const EdgeInsets.symmetric(
                           horizontal: 8, vertical: 5),
@@ -162,10 +260,10 @@ class _ChatScreenState extends State<PublicChatScreen> {
                                       ),
                                     ),
                                   if (!isMe) const SizedBox(height: 5),
-                                  Text(
-                                    text,
-                                    style: const TextStyle(color: Colors.white),
-                                  ),
+
+                                  buildMediaWidget(imageVideo,text)
+
+
                                 ],
                               ),
                             ),
@@ -201,15 +299,147 @@ class _ChatScreenState extends State<PublicChatScreen> {
                     onTap: _scrollToBottom,
                   ),
                 ),
+
+                IconButton(
+                  onPressed: imageUpload,
+                  icon: const Icon(Icons.image, color: Colors.grey),
+                ),
+
                 IconButton(
                   onPressed: _sendMessage,
                   icon: const Icon(Icons.send, color: Colors.blue),
                 ),
+
+
               ],
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+// Assuming imageVideo is a string that holds the file path or URL
+
+Widget buildMediaWidget(String imageVideo, String text) {
+  if (imageVideo.isEmpty) {
+    // If imageVideo is empty, show text
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.white),
+    );
+  } else if (imageVideo.contains(".mp4")) {
+    // If it's a video (contains .mp4), show the video player
+    return AnimalCardItem(imagePath: imageVideo);
+  } else {
+    // Otherwise, treat it as an image
+    return Image.network(storageURL+imageVideo);  // Or use Image.file() if it's a local path
+  }
+}
+
+class AnimalCardItem extends StatefulWidget {
+  final String imagePath;
+
+
+  const AnimalCardItem({required this.imagePath, super.key});
+
+  @override
+  State<AnimalCardItem> createState() => _AnimalCardItemState();
+}
+
+class _AnimalCardItemState extends State<AnimalCardItem> {
+  VideoPlayerController? _videoController;
+
+  void _initializeVideoControllerIfNeeded() async {
+    final videoUrl;
+    videoUrl = storageURL + widget.imagePath;
+
+    print(videoUrl);
+
+    if (widget.imagePath.contains(".mp4")) {
+      _videoController?.dispose();
+      _videoController = VideoPlayerController.network(videoUrl);
+
+      await _videoController!.initialize();
+      _videoController!.setLooping(true);
+
+      // if(widget.type=='1'){
+      //   _videoController!.play(); // Auto-play
+      //
+      // }
+
+      if (mounted) {
+        setState(() {});
+      }
+    } else {
+      _videoController?.dispose();
+      _videoController = null;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVideoControllerIfNeeded();
+  }
+
+  @override
+  void dispose() {
+    _videoController?.dispose();
+    super.dispose();
+  }
+
+  void _togglePlayPause() {
+    if (_videoController!.value.isPlaying) {
+      _videoController!.pause();
+    } else {
+      _videoController!.play();
+    }
+    setState(() {}); // Refresh UI
+  }
+
+  void _seekBy(Duration offset) {
+    final newPosition = _videoController!.value.position + offset;
+    _videoController!.seekTo(newPosition);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_videoController != null && _videoController!.value.isInitialized) {
+       return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              AspectRatio(
+                aspectRatio: _videoController!.value.aspectRatio,
+                child: VideoPlayer(_videoController!),
+              ),
+              IconButton(
+                icon: Icon(
+                  _videoController!.value.isPlaying
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                  color: Colors.white,
+                  size: 40,
+                ),
+                onPressed: _togglePlayPause,
+              ),
+            ],
+          ),
+          VideoProgressIndicator(
+            _videoController!,
+            allowScrubbing: true,
+            padding: const EdgeInsets.only(top: 6),
+          ),
+        ],
+      );
+
+    } else {
+      return const CircularProgressIndicator();
+    }
   }
 }
--- lib/utilities/const_res.dart
+++ lib/utilities/const_res.dart
@@ -1,5 +1,6 @@
 const String baseURL = 'https://drifttechnologies.in/';
 const String apiURL = '${baseURL}api/';
+const String storageURL = '${baseURL}public/storage/';
 const String apiKey = 'retry123';
 
 // If you change this topic you also change backend .env file
